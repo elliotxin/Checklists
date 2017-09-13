bcd48a52cdbb459ccf066a0e9662190bdc61d0ce
elliotxin <elliotxin@gmail.com>
2017-09-13 13:58:47 +0800

    Add Array for the project.

---
+++ Checklists/Checklists/ChecklistItem.swift
@@ -0,0 +1,19 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by elliot xin on 9/13/17.
+//  Copyright © 2017 elliot xin. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    
+    var text = ""
+    var checked = false
+    
+    func toggleChecked() {
+        checked = !checked
+    }
+}
