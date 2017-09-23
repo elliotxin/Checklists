6e2ca730e6951433b3caacaf65e3ff71bce733e3
elliotxin <elliotxin@gmail.com>
2017-09-23 11:40:16 +0800

    Using UserDefaults to remember stuff

--- Checklists/Checklists/Controller/AllListsViewController.swift
+++ Checklists/Checklists/Controller/AllListsViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController , ListDetailViewControllerDelegate{
+class AllListsViewController: UITableViewController , ListDetailViewControllerDelegate, UINavigationControllerDelegate{
 
     var dataModel: DataModel!
    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        navigationController?.delegate = self
+        let index = dataModel.indexOfSelectedChecklist
+        if index >= 0 && index < dataModel.lists.count {
+            let checklist = dataModel.lists[index]
+            performSegue(withIdentifier: "ShowChecklist", sender: checklist)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -60,7 +72,13 @@ class AllListsViewController: UITableViewController , ListDetailViewControllerDe
         }
     }
     
-    
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        
+        if viewController === self {
+           dataModel.indexOfSelectedChecklist = -1
+        }
+        
+    }
 }
 
 extension AllListsViewController{
@@ -80,6 +98,8 @@ extension AllListsViewController{
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        dataModel.indexOfSelectedChecklist = indexPath.row
         
         let checklist = dataModel.lists[indexPath.row]
         performSegue(withIdentifier: "ShowChecklist", sender: checklist)
--- Checklists/Checklists/Model/DataModel.swift
+++ Checklists/Checklists/Model/DataModel.swift
@@ -12,8 +12,21 @@ class DataModel {
     
     var lists = [Checklist]()
     
+    var indexOfSelectedChecklist: Int {
+        
+        get{
+            return UserDefaults.standard.integer(forKey: "ChecklistIndex")
+        }
+        set{
+            UserDefaults.standard.set(newValue, forKey: "ChecklistIndex")
+            UserDefaults.standard.synchronize()
+        }
+    }
+    
     init() {
         loadChecklists()
+        registerDefaults()
+        handleFirstTime()
     }
     
     func documentsDirectory() -> URL {
@@ -44,6 +57,27 @@ class DataModel {
             let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
             lists = unarchiver.decodeObject(forKey: "Checklists") as! [Checklist]
             unarchiver.finishDecoding()
+        }
+    }
+    
+    func registerDefaults(){
+        
+        let dictionary: [String: Any] = ["ChecklistIndex": -1, "FirstTime": true]
+        UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    func handleFirstTime(){
+        
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime{
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
         }
     }
 }
